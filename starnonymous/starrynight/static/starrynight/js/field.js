c1903890dab29aea37bd60530432122af1c122d3
grufelous <grufelous@gmail.com>
2019-02-06 15:52:26 +0530

    Fundamental front-end done - click bug, possibly due to DPI scaling. Django form up next

--- starnonymous/starrynight/static/starrynight/js/field.js
+++ starnonymous/starrynight/static/starrynight/js/field.js
@@ -1,1 +1,12 @@
-alert("Hello World");
+starContainer = document.getElementById("star-container");
+addEventListener("mousemove", function(mouse_event) {
+  posX = mouse_event.clientX+"px"
+  posY = mouse_event.clientY+"px"
+  document.getElementById('indicator').innerHTML = posX + ", " + posY
+  var new_star = document.createElement("P")
+  new_star.classList.add('star')
+  new_star.style.top = posY
+  new_star.style.left = posX
+  starContainer.append(new_star)
+  console.log(posX + ", " + posY)
+})
