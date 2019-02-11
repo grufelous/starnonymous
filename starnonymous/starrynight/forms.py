c13b512b62eccfe7d9b4ebd0d0671720c531d404
grufelous <grufelous@gmail.com>
2019-02-11 23:38:15 +0530

    Added ModelForms. Click issue yet to be solved

--- starnonymous/starrynight/forms.py
+++ starnonymous/starrynight/forms.py
@@ -1,4 +1,11 @@
 from django import forms
+from . import models
 
-class StarForm(forms.Form):
-    message = forms.CharField(max_length=512, widget=forms.TextInput(attrs={'placeholder': 'XD', 'class': 'message-in'}))
+class StarForm(forms.ModelForm):
+    star_label = forms.CharField(max_length=400, widget=forms.TextInput(attrs={'placeholder': 'XD', 'class': 'message-in'}), label='Sup nigga:')
+    star_time = forms.DateTimeField(widget=forms.SplitDateTimeWidget(attrs={'type': 'hidden'}))
+    xCoord = forms.CharField(max_length=12, widget=forms.TextInput(attrs={'type': 'hidden'}))
+    yCoord = forms.CharField(max_length=12, widget=forms.TextInput(attrs={'type': 'hidden'}))
+    class Meta:
+        model = models.Star
+        fields = '__all__'
